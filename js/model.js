00eb3f92b07233e57a17893d27b95ddc282823ea
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2023-02-11 18:39:41 +0100

    first commit

---
+++ js/model.js
@@ -0,0 +1,75 @@
+import { insertCountryObject, startGame } from "./script.js";
+// list of country from API, that according to different sources are NOT really country.
+const NOTCountry = [
+  "Svalbard and Jan Mayen",
+  "Guernsey",
+  "Isle of Man",
+  "Faroe Islands",
+  "Gibraltar",
+  "Jersey",
+  "Åland Islands",
+  "Réunion",
+  "Western Sahara",
+  "British Indian Ocean Territory",
+  "Saint Helena, Ascension and Tristan da Cunha",
+  "Mayotte",
+  "Hong Kong",
+  "Macau",
+  "United States Minor Outlying Islands",
+  "Cocos (Keeling) Islands",
+  "Norfolk Island",
+  "Solomon Islands",
+  "Pitcairn Islands",
+  "Christmas Island",
+];
+
+// fetch data
+const getJSON = function (url, errMsg = "Something went wrong") {
+  return fetch(url).then((response) => {
+    if (!response.ok) throw new Error(`${errMsg} (${response.status})`);
+    return response.json();
+  });
+};
+
+// https://restcountries.com/#api-endpoints-v3-region
+
+// liczba państw
+// europe 46
+// afryka 54
+// azja 48
+// ameryka 55
+// australia 22
+
+
+// search region from given array
+export const getCountries = async function (regionArr) {
+  try {
+    let dataToFetch = [];
+    regionArr.forEach((reg) =>
+      dataToFetch.push(getJSON(`https://restcountries.com/v3.1/region/${reg}`))
+    );
+
+    //fetch data for all choosen continents
+    const resultData = await Promise.all(dataToFetch);
+
+    resultData.map((data) => {
+      data.forEach((dat) => {
+        //delete NOT country array before adding to game object
+        let addCountry = true;
+        for (let i = 0; i < NOTCountry.length; i++)
+          if (NOTCountry[i] === dat.name.common) {
+            addCountry = false;
+            break;
+          }
+        if (addCountry) insertCountryObject(dat.name.common, dat.flags.svg);
+      });
+    });
+
+    // for (let i = 0; i < 1000000000; i++) {}
+
+    if (!resultData) throw new Error(`🔥 Data not found. 🔥`);
+    else startGame();
+  } catch (err) {
+    console.log(`🔥🔥 ${err} 🔥🔥`);
+  }
+};
---
+++ js/script.js
@@ -0,0 +1,359 @@
+"use strict";
+
+import { getCountries } from "./model.js";
+
+// selecting from DOM
+const svgMap = document.getElementById("svgMap");
+const header = document.getElementById("header");
+
+// const wrapper = document.querySelector(".wrapper");
+const questionWrapper = document.querySelector(".questionWrapper");
+const questionNumber = document.querySelector(".questionNumber");
+const loader = document.querySelector(".loader-container");
+const flagContainer = document.querySelector(".flag-img");
+const answerWrapper = document.querySelector(".answerWrapper");
+const scoreTableWrapper = document.querySelector(".scoreTableWrapper");
+const imgFlag = document.getElementById("flag");
+
+//answers buttons array
+const answerBtnA = document.getElementById("A");
+const answerBtnB = document.getElementById("B");
+const answerBtnC = document.getElementById("C");
+const answerBtnD = document.getElementById("D");
+const answersBtns = [answerBtnA, answerBtnB, answerBtnC, answerBtnD];
+
+//other buttons
+const btnPlay = document.querySelector(".play-btn-div");
+const btnResetGame = document.getElementById("resetGame");
+
+const init = function () {
+  //hide: map and btn
+  hideComponents([svgMap, btnPlay, header]);
+
+  //show loader till fetch ends
+  showComponents([questionWrapper, loader]);
+  questionWrapper.classList.add("display-flex");
+
+  //getting info about all continents from API
+  getCountries(game.selectedContinent);
+};
+
+//game - main object of quiz
+// selectedContinent - array of selected continents from world map
+// countryList - array of country and flags of player selected continent, if country is drawn to question - delete this country from this array
+//countryToAnswers - array of ALL country names selected continent to generate answers.always all country.
+// questions - array of object questions: answers Array, correctAnswer, flag, playerAnswer.
+let game = {
+  selectedContinent: [],
+  countryList: [],
+  countryToAnswers: [],
+  questions: [],
+};
+const QUESTION_QUANTITY = 1;
+const DELAY_BETWEEN_QUESTIONS_SEC = 1;
+let currQuestion = 0;
+
+// inserting country from fetch to array with country names and flags
+export const insertCountryObject = function (name, flag) {
+  game.countryList.push({
+    name: name,
+    flag: flag,
+  });
+  game.countryToAnswers.push(name);
+};
+
+// after continents are selected, start game: hide map, show form, fetch data, generate question,
+export const startGame = async function () {
+  //initial conditions for starting game
+  generateQuestions();
+  showQuestion();
+};
+
+// generate question from fetch API
+const generateQuestions = function () {
+  for (let i = 0; i < QUESTION_QUANTITY; i++) {
+    if (!game.countryList.length) return;
+    const randomCountryIndex = generateCountryRandomIndex(
+      game.countryList.length
+    );
+
+    //draw three random country to answers
+    let answers = [];
+    answers.push(game.countryList[randomCountryIndex].name);
+
+    // copy list all countries and delete country that is correct answer to random generate three other answers
+    // prettier-ignore
+    const copyCountries = game.countryToAnswers.filter(
+      (country) => country !== answers[0]
+    );
+
+    const randomSet = generateThreeRandomIndex(copyCountries.length);
+    randomSet.forEach((ans) => {
+      return answers.push(copyCountries[ans]);
+    });
+
+    // shuffle answers array
+    answers = shuffleArray(answers);
+    // push question to array
+    game.questions.push({
+      flag: game.countryList[randomCountryIndex].flag,
+      answers: answers,
+      correctAnswer: game.countryList[randomCountryIndex].name,
+    });
+
+    //remove draw counrty from list
+    game.countryList.splice(randomCountryIndex, 1);
+  }
+};
+
+// start showing form with questions
+const showQuestion = function () {
+  // get answer buttons from DOM
+  // prettier-ignore
+  questionNumber.textContent = `Question ${currQuestion + 1} of ${QUESTION_QUANTITY}\n`;
+
+  // adding flag to DOM
+  showComponents([loader]);
+  imgFlag.src = game.questions[currQuestion].flag;
+  imgFlag.addEventListener("load", hideComponents([loader]));
+
+  // displaing answers to all buttons
+  answersBtns.forEach((ans, index) => {
+    ans.innerHTML = game.questions[currQuestion].answers[index];
+  });
+
+  // hideComponents([loader]);
+  showComponents([btnResetGame, flagContainer, answerWrapper]);
+};
+
+// checking player answer
+const checkAnswer = function (playerAnswer, playerAnswerId) {
+  const correctAnswer = game.questions[currQuestion].correctAnswer;
+
+  //give all buttons disabled atribute
+  answersBtns.map((btn) => {
+    btn.className = "";
+    btn.classList.add("btnQuestionDisabled");
+    btn.disabled = true;
+  });
+
+  // player answer is correct
+  if (playerAnswer === correctAnswer) {
+    //adding correct (true) answer to question array
+    game.questions[currQuestion].playerAnswer = true;
+
+    // indication correct answer to player
+    const goodAnswerBtn = document.getElementById(playerAnswerId);
+    goodAnswerBtn.className = "";
+    goodAnswerBtn.classList.add("btnQuestionCorrect");
+  } else {
+    //player answer is wrong
+    //adding wrong (false) answer to question array
+    game.questions[currQuestion].playerAnswer = false;
+
+    // indication wrong answer to player
+    const wrongAnswerBtn = document.getElementById(playerAnswerId);
+    wrongAnswerBtn.className = "";
+    wrongAnswerBtn.classList.add("btnQuestionWrong");
+
+    // searching and indication correct answer to player
+    answersBtns.map((ansBtn) => {
+      if (ansBtn.innerHTML === correctAnswer) {
+        ansBtn.className = "";
+        ansBtn.classList.add("btnQuestionCorrect");
+      }
+    });
+  }
+  timeout(DELAY_BETWEEN_QUESTIONS_SEC);
+};
+
+// player answer to all question - display summary table
+const gameOver = function () {
+  const scoreTable = document.querySelector(".scoreTable");
+
+  showComponents([scoreTableWrapper]);
+  hideComponents([btnResetGame]);
+
+  //calculating player score
+  const countCorrectAnswer = game.questions.filter(
+    (ques) => ques.playerAnswer
+  ).length;
+  const countWrongAnswer = QUESTION_QUANTITY - countCorrectAnswer;
+
+  const percent = Math.floor(
+    (countCorrectAnswer * 100) / QUESTION_QUANTITY
+  ).toFixed(0);
+
+  let html = `
+    <div class="gameOver">Game over.</div>
+    <div>Your score : <span>${percent}%</span></div>
+    <div class="questionQuantity">Number of questions : <span>${QUESTION_QUANTITY}</span></div>
+    <div class="good">Good answers : <span>${countCorrectAnswer}</span></div>
+    <div class="wrong">Wrong answers : <span>${countWrongAnswer}</span></div>
+  `;
+
+  html += `
+      <div class="playAgain-btn">
+      <button class="play-btn">Play again
+        <div class="icon">
+          <svg id="svg-start-game"         
+          viewBox="0 0 24 24" stroke-width="1.5" ><defs><style>.cls-637b8a2bf95e86b59c57a23b-1{fill:none;stroke:currentColor;stroke-miterlimit:10;}</style></defs><path class="cls-637b8a2bf95e86b59c57a23b-1" d="M18.34,4.21A4.86,4.86,0,0,0,14,6.14H10A4.86,4.86,0,0,0,5.66,4.21,5,5,0,0,0,1.25,9.28v7A3.5,3.5,0,0,0,8,17.61c.42-1.05.94-2.37,1.42-3.66h5.16c.48,1.29,1,2.61,1.42,3.66a3.5,3.5,0,0,0,6.75-1.29v-7A5,5,0,0,0,18.34,4.21Z"></path><path class="cls-637b8a2bf95e86b59c57a23b-1" d="M18.84,12h0Z"></path><path class="cls-637b8a2bf95e86b59c57a23b-1" d="M18.84,8.09h0Z"></path><path class="cls-637b8a2bf95e86b59c57a23b-1" d="M20.79,10.05h0Z"></path><path class="cls-637b8a2bf95e86b59c57a23b-1" d="M16.89,10.05h0Z"></path><path class="cls-637b8a2bf95e86b59c57a23b-1" d="M7.11,12h0Z"></path><path class="cls-637b8a2bf95e86b59c57a23b-1" d="M7.11,8.09h0Z"></path><path class="cls-637b8a2bf95e86b59c57a23b-1" d="M9.07,10.05h0Z"></path><path class="cls-637b8a2bf95e86b59c57a23b-1" d="M5.16,10.05h0Z"></path></svg>
+        </div>
+      </button>
+    </div>
+`;
+
+  scoreTable.innerHTML = html;
+
+  // after select continents - start game
+  document.querySelector(".playAgain-btn").addEventListener("click", resetGame);
+};
+
+//reset game :)
+const resetGame = function () {
+  imgFlag.src = "";
+
+  // show world map
+  showComponents([svgMap, btnPlay, header]);
+
+  // loop selected continent for deselect from svg map
+  game.selectedContinent.map((con) => {
+    const querySelAll = document.querySelectorAll(`.${con}`);
+    querySelAll.forEach((child) => {
+      child.classList.toggle("selected");
+    });
+  });
+
+  //hiddin page componets
+  questionWrapper.classList.remove("display-flex");
+  hideComponents([questionWrapper, btnPlay, btnResetGame, scoreTableWrapper]);
+
+  //reseting answer button classes
+  resetBtnClass();
+
+  // setting initial conditions for game object
+  game = {
+    selectedContinent: [],
+    countryList: [],
+    countryToAnswers: [],
+    questions: [],
+  };
+  currQuestion = 0;
+};
+
+//selecting continent on world map
+const selectContinent = function (clickContinent) {
+  // select class of clicked continent
+  const querySelAll = document.querySelectorAll(`.${clickContinent}`);
+  querySelAll.forEach((child) => {
+    child.classList.toggle("selected");
+
+    //add or remove from selectedContinent array
+    if (child.classList.contains("selected")) {
+      game.selectedContinent.indexOf(clickContinent) === -1 &&
+        game.selectedContinent.push(clickContinent);
+    } else {
+      game.selectedContinent.indexOf(clickContinent) > -1 &&
+        game.selectedContinent.splice(
+          game.selectedContinent.indexOf(clickContinent),
+          1
+        );
+    }
+  });
+};
+
+// shuffle array of answers to display
+const shuffleArray = function (array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+};
+
+// according to length of given array generate random number
+const generateCountryRandomIndex = function (arrLength) {
+  return Math.trunc(Math.random() * arrLength) + 1;
+};
+
+//generate three other answer - others country to question
+const generateThreeRandomIndex = function (length) {
+  const randomSet = new Set();
+  do {
+    let ran = Math.trunc(Math.random() * length);
+    randomSet.add(ran);
+  } while (randomSet.size < 3);
+  return randomSet;
+};
+
+// reset all answer button class to normal
+const resetBtnClass = function () {
+  answersBtns.map((btn) => {
+    btn.className = "";
+    btn.classList.add("btnQuestion");
+    btn.disabled = false;
+  });
+};
+
+// timeout between giving answer and switch to next question
+const timeout = function (s) {
+  setTimeout(function () {
+    if (currQuestion + 1 < QUESTION_QUANTITY) {
+      currQuestion++;
+      showQuestion();
+      resetBtnClass();
+    } else gameOver();
+  }, s * 1000);
+};
+
+// remove class hidden from given components
+const showComponents = function (components) {
+  components.forEach((comp) => {
+    // console.log("remove:", comp);
+    comp.classList.remove("hidden");
+  });
+};
+
+// add class hidden from given components
+const hideComponents = function (components) {
+  components.forEach((comp) => {
+    // console.log("add:", comp);
+    comp.classList.add("hidden");
+  });
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// EVENT LISTENERS
+// world map - select continents
+svgMap.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  //if not click on continent
+  if (e.target.id === "svgMap") return;
+
+  // get id (Africa, Europe etc) of selected continent
+  const clickContinent = e.target.closest(".continent").id;
+
+  //search all nodes
+  selectContinent(clickContinent);
+
+  // show or hide start game btn
+
+  if (game.selectedContinent.length > 0) showComponents([btnPlay]);
+  if (game.selectedContinent.length === 0) hideComponents([btnPlay]);
+});
+
+// after select continents - start game
+btnPlay.addEventListener("click", init);
+
+// player choose answer - check answer
+answerWrapper.addEventListener("click", function (e) {
+  const answer = e.target.innerHTML;
+  const answerId = e.target.id;
+  if (!answer || !answerId) return;
+  checkAnswer(answer, answerId);
+});
+
+btnResetGame.addEventListener("click", resetGame);
